0bed08e20ed98a362016ed35c604031abba127a9
David Zeller <david.zeller@outlook.com>
2020-08-04 19:47:45 +0200

    Initial Copy

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp8Test
+{
+    
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+
+        public readonly override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point { X=4, Y=8};
+            Console.WriteLine(p.ToString());
+            Console.ReadLine();
+        }
+    }
+}
